7a1355d531d9fc0129358773fee55d62bbf9b1b5
Justin Moss <Justin.Moss@gmail.com>
2019-10-10 11:55:25 -0400

    Move NetCoreResponseWriter example to .Net Core 2.2 instead of 3

--- Examples/NetCoreResponseWriter/Startup.cs
+++ Examples/NetCoreResponseWriter/Startup.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using RockLib.HealthChecks.AspNetCore.ResponseWriter;
 using HealthChecks.System;
 
@@ -19,23 +18,18 @@ namespace NetCoreResponseWriter
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
             RockLibHealthChecks.Indent = true;
 
-            app.UseEndpoints(endpoints =>
+            app.UseHealthChecks("/health", new HealthCheckOptions
             {
-                endpoints.MapHealthChecks("/health", new HealthCheckOptions
-                {
-                    ResponseWriter = RockLibHealthChecks.ResponseWriter
-                });
+                ResponseWriter = RockLibHealthChecks.ResponseWriter
             });
         }
     }
