901f4f6d47afaa599c3149eb50db062e55d924ab
Brian Friesen <brian.friesen@gmail.com>
2019-12-12 18:44:53 -0500

    Update AspNetCore project to use DependencyInjection project

--- RockLib.HealthChecks.AspNetCore/HealthCheckMiddleware.cs
+++ RockLib.HealthChecks.AspNetCore/HealthCheckMiddleware.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace RockLib.HealthChecks.AspNetCore
@@ -10,6 +13,27 @@ namespace RockLib.HealthChecks.AspNetCore
     {
         private readonly IHealthCheckRunner _healthCheckRunner;
         private readonly bool _indent;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HealthCheckMiddleware"/> class.
+        /// </summary>
+        /// <param name="next">
+        /// Ignored. Required to exist in the constructor in order to meet the definition of a middleware.
+        /// </param>
+        /// <param name="healthCheckRunners">
+        /// A collection of <see cref="IHealthCheckRunner"/> objects. The one whose <see cref="IHealthCheckRunner.Name"/>
+        /// matches the <paramref name="healthCheckRunnerName"/> parameter is the one used by this middleware.
+        /// </param>
+        /// <param name="healthCheckRunnerName">The name of the health check runner to use.</param>
+        /// <param name="indent">Whether to indent the JSON output.</param>
+        /// <remarks>
+        /// This constructor exists primarily so that the <see cref="HealthCheckMiddleware"/> class "plays nice" with
+        /// dependency injection.
+        /// </remarks>
+        public HealthCheckMiddleware(RequestDelegate next, IEnumerable<IHealthCheckRunner> healthCheckRunners, string healthCheckRunnerName, bool indent = false)
+            : this(next, GetHealthCheckRunner(healthCheckRunners?.ToList() ?? throw new ArgumentNullException(nameof(healthCheckRunners)), healthCheckRunnerName), indent)
+        {
+        }
 
 #pragma warning disable IDE0060 // Remove unused parameter
         /// <summary>
@@ -27,7 +51,7 @@ namespace RockLib.HealthChecks.AspNetCore
         {
             // This is a terminal middleware, so throw away the RequestDelegate.
 
-            _healthCheckRunner = healthCheckRunner;
+            _healthCheckRunner = healthCheckRunner ?? HealthCheck.GetRunner();
             _indent = indent;
         }
 
@@ -37,14 +61,28 @@ namespace RockLib.HealthChecks.AspNetCore
         /// <param name="context">The HTTP context for the current request.</param>
         public async Task InvokeAsync(HttpContext context)
         {
-            var healthCheckResponse = await _healthCheckRunner.RunAsync(context.RequestAborted).ConfigureAwait(false);
+            var healthResponse = await _healthCheckRunner.RunAsync(context.RequestAborted).ConfigureAwait(false);
 
-            context.Response.StatusCode = healthCheckResponse.StatusCode;
-            context.Response.ContentType = healthCheckResponse.ContentType;
+            context.Response.StatusCode = healthResponse.StatusCode;
+            context.Response.ContentType = healthResponse.ContentType;
 
-            await context.Response.WriteAsync(healthCheckResponse.Serialize(_indent)).ConfigureAwait(false);
+            await context.Response.WriteAsync(healthResponse.Serialize(_indent)).ConfigureAwait(false);
 
             // Terminal middlewares don't invoke the 'next' delegate.
         }
+
+        private static IHealthCheckRunner GetHealthCheckRunner(IEnumerable<IHealthCheckRunner> healthCheckRunners, string healthCheckRunnerName)
+        {
+            var runners = healthCheckRunners.ToList();
+
+            if (runners.Count == 0)
+                return HealthCheck.GetRunner(healthCheckRunnerName);
+
+            healthCheckRunnerName = GetName(healthCheckRunnerName);
+            return runners.SingleOrDefault(runner => healthCheckRunnerName.Equals(GetName(runner.Name)))
+                ?? throw new InvalidOperationException($"One or more health check runners were provided, but none match the specified name, '{healthCheckRunnerName}'.");
+        }
+
+        private static string GetName(string name) => string.IsNullOrEmpty(name) ? "default" : name;
     }
 }
--- RockLib.HealthChecks.AspNetCore/HealthCheckMiddlewareExtensions.cs
+++ RockLib.HealthChecks.AspNetCore/HealthCheckMiddlewareExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using System;
 
 namespace RockLib.HealthChecks.AspNetCore
 {
@@ -13,38 +14,25 @@ namespace RockLib.HealthChecks.AspNetCore
         /// </summary>
         /// <param name="builder">The application builder.</param>
         /// <param name="healthCheckRunnerName">
-        /// The name of the <see cref="IHealthCheckRunner"/> to use. If <see langword="null"/> or not provided,
-        /// the default value of the <see cref="HealthCheck.GetRunner"/> method is used.
+        /// The name of the health runner that will perform health checks for the health endpoint.
         /// </param>
         /// <param name="route">The route of the health endpoint.</param>
         /// <param name="indent">Whether to indent the JSON output.</param>
         /// <returns>The application builder.</returns>
         public static IApplicationBuilder UseRockLibHealthChecks(this IApplicationBuilder builder,
-            string healthCheckRunnerName = null, string route = "/health", bool indent = false)
-            => builder.UseRockLibHealthChecks(HealthCheck.GetRunner(healthCheckRunnerName), route, indent);
-
-        /// <summary>
-        /// Adds a terminal <see cref="HealthCheckMiddleware"/> to the application.
-        /// </summary>
-        /// <param name="builder">The application builder.</param>
-        /// <param name="healthCheckRunner">
-        /// The <see cref="IHealthCheckRunner"/> to use. If <see langword="null"/>,
-        /// the value of the <see cref="HealthCheck.GetRunner"/> method is used.
-        /// </param>
-        /// <param name="route">The route of the health endpoint.</param>
-        /// <param name="indent">Whether to indent the JSON output.</param>
-        /// <returns>The application builder.</returns>
-        public static IApplicationBuilder UseRockLibHealthChecks(this IApplicationBuilder builder,
-            IHealthCheckRunner healthCheckRunner, string route = "/health", bool indent = false)
+            string healthCheckRunnerName = "", string route = "/health", bool indent = false)
         {
-            healthCheckRunner = healthCheckRunner ?? HealthCheck.GetRunner();
-            route = $"/{route.Trim('/')}";
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+            if (healthCheckRunnerName == null)
+                throw new ArgumentNullException(nameof(healthCheckRunnerName));
+            if (route == null)
+                throw new ArgumentNullException(nameof(route));
 
-            return builder
-                .Map(new PathString(route), appBuilder =>
-                {
-                    appBuilder.UseMiddleware<HealthCheckMiddleware>(healthCheckRunner, indent);
-                });
+            var path = new PathString($"/{route.Trim('/')}");
+
+            return builder.Map(path, appBuilder =>
+                appBuilder.UseMiddleware<HealthCheckMiddleware>(healthCheckRunnerName, indent));
         }
     }
 }
