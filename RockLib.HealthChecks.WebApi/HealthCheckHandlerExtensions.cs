b4376d9677553898f3fb919682b81435e963a01d
Jason Bock <jason.r.bock@outlook.com>
2022-12-14 12:43:02 -0600

    Modernized Library and Added Tests (#31)
    * #160 modernized library and added tests
    
    * #160 manually added regex package to try and address snyk issue
    
    * #160 updated tests and added to changelog
    
    Co-authored-by: Jason Bock <JasonBock1@rocketmortgage.com>

--- RockLib.HealthChecks.WebApi/HealthCheckHandlerExtensions.cs
+++ RockLib.HealthChecks.WebApi/HealthCheckHandlerExtensions.cs
@@ -2,55 +2,62 @@
 using System.Threading;
 using System.Web.Http;
 
-namespace RockLib.HealthChecks.WebApi
+namespace RockLib.HealthChecks.WebApi;
+
+/// <summary>
+/// Extensions for adding a RockLib.HealthChecks route to a route collection.
+/// </summary>
+public static class HealthCheckHandlerExtensions
 {
+    private static int _routeIndex = -1;
+
     /// <summary>
-    /// Extensions for adding a RockLib.HealthChecks route to a route collection.
+    /// Adds a health check route to the route collection.
     /// </summary>
-    public static class HealthCheckHandlerExtensions
+    /// <param name="routes">The route collection.</param>
+    /// <param name = "healthCheckRunnerName" >
+    /// The name of the <see cref="IHealthCheckRunner"/> to use. If <see langword="null"/> or not provided,
+    /// the default value of the <see cref="HealthCheck.GetRunner"/> method is used.
+    /// </param>
+    /// <param name="route">The route of the health endpoint.</param>
+    /// <param name="indent">Whether to indent the JSON output.</param>
+    public static void MapHealthRoute(this HttpRouteCollection routes,
+        string? healthCheckRunnerName = null, string route = "/health", bool indent = false)
+        => routes.MapHealthRoute(HealthCheck.GetRunner(healthCheckRunnerName)!, route, indent);
+
+
+    /// <summary>
+    /// Adds a health check route to the route collection.
+    /// </summary>
+    /// <param name="routes">The route collection.</param>
+    /// <param name="healthCheckRunner">
+    /// The <see cref="IHealthCheckRunner"/> to use. If <see langword="null"/>,
+    /// the value of the <see cref="HealthCheck.GetRunner"/> method is used.
+    /// </param>
+    /// <param name="route">The route of the health endpoint.</param>
+    /// <param name="indent">Whether to indent the JSON output.</param>
+    public static void MapHealthRoute(this HttpRouteCollection routes,
+        IHealthCheckRunner healthCheckRunner, string route = "/health", bool indent = false)
     {
-        private static int _routeIndex = -1;
+#if NET6_0_OR_GREATER
+        ArgumentNullException.ThrowIfNull(routes);
+#else
+        if (routes is null) { throw new ArgumentNullException(nameof(routes)); }
+#endif
 
-        /// <summary>
-        /// Adds a health check route to the route collection.
-        /// </summary>
-        /// <param name="routes">The route collection.</param>
-        /// <param name = "healthCheckRunnerName" >
-        /// The name of the <see cref="IHealthCheckRunner"/> to use. If <see langword="null"/> or not provided,
-        /// the default value of the <see cref="HealthCheck.GetRunner"/> method is used.
-        /// </param>
-        /// <param name="route">The route of the health endpoint.</param>
-        /// <param name="indent">Whether to indent the JSON output.</param>
-        public static void MapHealthRoute(this HttpRouteCollection routes,
-            string healthCheckRunnerName = null, string route = "/health", bool indent = false)
-            => routes.MapHealthRoute(HealthCheck.GetRunner(healthCheckRunnerName), route, indent);
-        
-
-        /// <summary>
-        /// Adds a health check route to the route collection.
-        /// </summary>
-        /// <param name="routes">The route collection.</param>
-        /// <param name="healthCheckRunner">
-        /// The <see cref="IHealthCheckRunner"/> to use. If <see langword="null"/>,
-        /// the value of the <see cref="HealthCheck.GetRunner"/> method is used.
-        /// </param>
-        /// <param name="route">The route of the health endpoint.</param>
-        /// <param name="indent">Whether to indent the JSON output.</param>
-        public static void MapHealthRoute(this HttpRouteCollection routes,
-            IHealthCheckRunner healthCheckRunner, string route = "/health", bool indent = false)
+        if (string.IsNullOrWhiteSpace(route))
         {
-            if (routes == null)
-                throw new ArgumentNullException(nameof(routes));
-            if (string.IsNullOrWhiteSpace(route))
-                throw new ArgumentNullException(nameof(route));
-
-            healthCheckRunner = healthCheckRunner ?? HealthCheck.GetRunner();
-            route = route.Trim('/');
-
-            var routeIndex = Interlocked.Increment(ref _routeIndex);
-            var routeName = routeIndex < 1 ? "HealthApi" : $"HealthApi-{routeIndex}";
-
-            routes.MapHttpRoute(routeName, route, null, null, new HealthCheckHandler(healthCheckRunner, indent));
+            throw new ArgumentException("The route parameter has to contain a value.", nameof(route));
         }
+
+        healthCheckRunner ??= HealthCheck.GetRunner()!;
+        route = route.Trim('/');
+
+        var routeIndex = Interlocked.Increment(ref _routeIndex);
+        var routeName = routeIndex < 1 ? "HealthApi" : $"HealthApi-{routeIndex}";
+
+#pragma warning disable CA2000 // Dispose objects before losing scope
+        routes.MapHttpRoute(routeName, route, null, null, new HealthCheckHandler(healthCheckRunner, indent));
+#pragma warning restore CA2000 // Dispose objects before losing scope
     }
 }
---
+++ Tests/RockLib.HealthChecks.WebApi.Tests/HealthCheckHandlerExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using System;
+using System.Web.Http;
+using Xunit;
+
+namespace RockLib.HealthChecks.WebApi.Tests;
+
+public static class HealthCheckHandlerExtensionsTests
+{
+    [Fact]
+    public static void MapHealthRoute()
+    {
+        using var routes = new HttpRouteCollection();
+        routes.MapHealthRoute(Mock.Of<IHealthCheckRunner>());
+
+        Assert.Multiple(
+            () => Assert.Single(routes),
+            () => Assert.IsType<HealthCheckHandler>(routes["HealthApi"].Handler)
+            );
+    }
+
+    [Fact]
+    public static void MapHealthRouteWhenRoutesIsNull() => 
+        Assert.Throws<ArgumentNullException>(
+            () => (null as HttpRouteCollection)!.MapHealthRoute(Mock.Of<IHealthCheckRunner>()));
+
+    [Fact]
+    public static void MapHealthRouteWhenRouteIsNull()
+    {
+        using var routes = new HttpRouteCollection();
+        Assert.Throws<ArgumentException>(
+            () => routes.MapHealthRoute(Mock.Of<IHealthCheckRunner>(), route: null!));
+    }
+
+    [Fact]
+    public static void MapHealthRouteWhenRouteIsEmpty()
+    {
+        using var routes = new HttpRouteCollection();
+        Assert.Throws<ArgumentException>(
+            () => routes.MapHealthRoute(Mock.Of<IHealthCheckRunner>(), route: string.Empty));
+    }
+}
