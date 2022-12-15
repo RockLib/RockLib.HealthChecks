b5e26f3905f61820ee03f1fc926b16159bae8fdf
Jason Bock <jason.r.bock@outlook.com>
2022-12-15 08:08:27 -0600

    Modernize ResponseWriter (#33)
    * #157 modernized ResponseWriter project
    
    * #157 modernization complete
    
    * #157 updated changelog for release
    
    Co-authored-by: Jason Bock <JasonBock1@rocketmortgage.com>

--- RockLib.HealthChecks.AspNetCore.ResponseWriter/RockLibHealthChecks.cs
+++ RockLib.HealthChecks.AspNetCore.ResponseWriter/RockLibHealthChecks.cs
@@ -7,122 +7,116 @@ using System.Linq;
 using System.Threading.Tasks;
 using MSHealthStatus = Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus;
 
-namespace RockLib.HealthChecks.AspNetCore.ResponseWriter
+namespace RockLib.HealthChecks.AspNetCore.ResponseWriter;
+
+/// <summary>
+/// Defines a delegate that maps its <see cref="HealthReport"/> parameter to the <see cref="HealthResponse"/>
+/// type and writes it to its <see cref="HttpContext.Response"/> parameter. Intended to be used to set the value
+/// of the <see cref="HealthCheckOptions.ResponseWriter"/> property.
+/// </summary>
+public static class RockLibHealthChecks
 {
     /// <summary>
-    /// Defines a delegate that maps its <see cref="HealthReport"/> parameter to the <see cref="HealthResponse"/>
-    /// type and writes it to its <see cref="HttpContext.Response"/> parameter. Intended to be used to set the value
-    /// of the <see cref="HealthCheckOptions.ResponseWriter"/> property.
+    /// Gets or sets a value indicating whether the <see cref="ResponseWriter"/> delegate will indent
+    /// its JSON output.
     /// </summary>
-    public static class RockLibHealthChecks
+    public static bool Indent { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the <see cref="ResponseWriter"/> delegate will display
+    /// the exception property in its JSON output.
+    /// The default is to show the exception.
+    /// </summary>
+    public static bool HideExceptions { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the <see cref="ResponseWriter"/> delegate will hide
+    /// the output property in its JSON output.
+    /// The default is to show the output.
+    /// </summary>
+    public static bool HideOutputs { get; set; }
+
+    /// <summary>
+    /// Gets a delegate that maps its <see cref="HealthReport"/> parameter to the <see cref="HealthResponse"/>
+    /// type and writes it to its <see cref="HttpContext.Response"/> parameter. Intended to be used to set the
+    /// value of the <see cref="HealthCheckOptions.ResponseWriter"/> property.
+    /// </summary>
+    public static Func<HttpContext, HealthReport, Task> ResponseWriter { get; } = WriteResponse;
+
+    private static async Task WriteResponse(HttpContext httpContext, HealthReport healthReport)
     {
-        /// <summary>
-        /// Gets or sets a value indicating whether the <see cref="ResponseWriter"/> delegate will indent
-        /// its JSON output.
-        /// </summary>
-        public static bool Indent { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether the <see cref="ResponseWriter"/> delegate will display
-        /// the exception property in its JSON output.
-        /// The default is to show the exception.
-        /// </summary>
-        public static bool HideExceptions { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether the <see cref="ResponseWriter"/> delegate will hide
-        /// the output property in its JSON output.
-        /// The default is to show the output.
-        /// </summary>
-        public static bool HideOutputs { get; set; }
-
-        /// <summary>
-        /// Gets a delegate that maps its <see cref="HealthReport"/> parameter to the <see cref="HealthResponse"/>
-        /// type and writes it to its <see cref="HttpContext.Response"/> parameter. Intended to be used to set the
-        /// value of the <see cref="HealthCheckOptions.ResponseWriter"/> property.
-        /// </summary>
-        public static Func<HttpContext, HealthReport, Task> ResponseWriter { get; } = WriteResponse;
-
-        private async static Task WriteResponse(HttpContext httpContext, HealthReport healthReport)
+        var results = healthReport.Entries.Select(x =>
         {
-            var results = healthReport.Entries.Select(x =>
+            var entry = x.Value;
+
+            var result = new HealthCheckResult
             {
-                var entry = x.Value;
-
-                var result = new HealthCheckResult
-                {
-                    ComponentName = GetComponentName(x.Key),
-                    MeasurementName = GetMeasurementName(x.Key),
-                    Status = MapStatus(entry.Status),
-                    ["duration"] = entry.Duration
-                };
-
-                if (!HideExceptions)
-                    result["exception"] = entry.Exception?.ToString();
-
-                if (!HideOutputs)
-                    result.Output = entry.Description;
-
-                // TODO: Detect collisions between entry.Exception/result.Output,
-                // entry.Description/result["description", or entry.Duration/result["duration"].
-
-                foreach (var data in entry.Data)
-                    result[data.Key] = data.Value;
-
-                return result;
-            }).ToList();
-
-            var response = new HealthResponse(results)
-            {
-                Status = MapStatus(healthReport.Status),
-                Notes = new List<string> { $"TotalDuration: {healthReport.TotalDuration}" }
+                ComponentName = GetComponentName(x.Key),
+                MeasurementName = GetMeasurementName(x.Key),
+                Status = MapStatus(entry.Status),
+                ["duration"] = entry.Duration
             };
 
-            httpContext.Response.ContentType = response.ContentType;
-            await httpContext.Response.WriteAsync(response.Serialize(Indent));
-        }
-
-        private static HealthStatus MapStatus(MSHealthStatus status)
-        {
-            switch (status)
+            if (!HideExceptions)
             {
-                case MSHealthStatus.Unhealthy:
-                    return HealthStatus.Fail;
-                case MSHealthStatus.Degraded:
-                    return HealthStatus.Warn;
-                case MSHealthStatus.Healthy:
-                    return HealthStatus.Pass;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(status));
+                result["exception"] = entry.Exception?.ToString();
             }
-        }
 
-        private static string GetComponentName(string key)
-        {
-            var split = key.Split(':');
-
-            switch (split.Length)
+            if (!HideOutputs)
             {
-                case 1:
-                    return null;
-                default:
-                    return split[0];
+                result.Output = entry.Description;
             }
-        }
 
-        private static string GetMeasurementName(string key)
-        {
-            var split = key.Split(':');
+            // TODO: Detect collisions between entry.Exception/result.Output,
+            // entry.Description/result["description", or entry.Duration/result["duration"].
 
-            switch (split.Length)
+            foreach (var data in entry.Data)
             {
-                case 1:
-                    return split[0];
-                case 2:
-                    return split[1];
-                default:
-                    return string.Join("", split.Skip(1));
+                result[data.Key] = data.Value;
             }
-        }
+
+            return result;
+        }).ToList();
+
+        var response = new HealthResponse(results)
+        {
+            Status = MapStatus(healthReport.Status),
+            Notes = new List<string> { $"TotalDuration: {healthReport.TotalDuration}" }
+        };
+
+        httpContext.Response.ContentType = response.ContentType;
+        await httpContext.Response.WriteAsync(response.Serialize(Indent)).ConfigureAwait(false);
+    }
+
+    private static HealthStatus MapStatus(MSHealthStatus status) => 
+        status switch
+        {
+            MSHealthStatus.Unhealthy => HealthStatus.Fail,
+            MSHealthStatus.Degraded => HealthStatus.Warn,
+            MSHealthStatus.Healthy => HealthStatus.Pass,
+            _ => throw new ArgumentOutOfRangeException(nameof(status)),
+        };
+
+    private static string? GetComponentName(string key)
+    {
+        var split = key.Split(':');
+
+        return split.Length switch
+        {
+            1 => null,
+            _ => split[0],
+        };
+    }
+
+    private static string GetMeasurementName(string key)
+    {
+        var split = key.Split(':');
+
+        return split.Length switch
+        {
+            1 => split[0],
+            2 => split[1],
+            _ => string.Join("", split.Skip(1)),
+        };
     }
 }
--- Tests/RockLib.HealthChecks.AspNetCore.ResponseWriter.Tests/ResponseWriterTests.cs
+++ Tests/RockLib.HealthChecks.AspNetCore.ResponseWriter.Tests/ResponseWriterTests.cs
@@ -1,171 +1,173 @@
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoFixture.Xunit2;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
-namespace RockLib.HealthChecks.AspNetCore.ResponseWriter.Tests
+namespace RockLib.HealthChecks.AspNetCore.ResponseWriter.Tests;
+
+// Since we're testing static members on a static class,
+// we shouldn't run them in paralle.
+public static class ResponseWriterTests
 {
-    public class ResponseWriterTests
+    [Theory]
+    [AutoData]
+    public static async Task ResponseWriterHideExceptionsDefaultShouldShowExceptionsProperty(
+        HealthReport healthReport)
     {
-        [Theory]
-        [AutoData]
-        public async Task ResponseWriter_HideExceptions_Default_ShouldShowExceptionsProperty(
-            HealthReport healthReport)
-        {
-            // Arrange
-            var httpContext = CreateHttpContext();
+        // Arrange
+        var httpContext = CreateHttpContext();
 
-            // Act
-            await RockLibHealthChecks.ResponseWriter(httpContext, healthReport);
+        // Act
+        await RockLibHealthChecks.ResponseWriter(httpContext, healthReport).ConfigureAwait(false);
 
-            // Assert
-            var jsonBody = await GetJsonBody(httpContext);
-            var healthCheckEntries = GetHealthCheckEntries(jsonBody);
+        // Assert
+        var jsonBody = await GetJsonBody(httpContext).ConfigureAwait(false);
+        var healthCheckEntries = GetHealthCheckEntries(jsonBody);
 
-            healthCheckEntries
-                .Any(x => x["exception"] != null)
-                .Should()
-                .BeTrue();
-        }
+        healthCheckEntries
+            .Any(x => x["exception"] != null)
+            .Should()
+            .BeTrue();
+    }
 
-        [Theory]
-        [AutoData]
-        public async Task ResponseWriter_HideExceptions_SetToFalse_ShouldShowExceptionsProperty(
-            HealthReport healthReport)
-        {
-            // Arrange
-            var httpContext = CreateHttpContext();
-            RockLibHealthChecks.HideExceptions = false;
+    [Theory]
+    [AutoData]
+    public static async Task ResponseWriterHideExceptionsSetToFalseShouldShowExceptionsProperty(
+        HealthReport healthReport)
+    {
+        // Arrange
+        var httpContext = CreateHttpContext();
+        RockLibHealthChecks.HideExceptions = false;
 
-            // Act
-            await RockLibHealthChecks.ResponseWriter(httpContext, healthReport);
+        // Act
+        await RockLibHealthChecks.ResponseWriter(httpContext, healthReport).ConfigureAwait(false);
 
-            // Assert
-            var jsonBody = await GetJsonBody(httpContext);
-            var healthCheckEntries = GetHealthCheckEntries(jsonBody);
+        // Assert
+        var jsonBody = await GetJsonBody(httpContext).ConfigureAwait(false);
+        var healthCheckEntries = GetHealthCheckEntries(jsonBody);
 
-            healthCheckEntries
-                .Any(x => x["exception"] != null)
-                .Should()
-                .BeTrue();
-        }
+        healthCheckEntries
+            .Any(x => x["exception"] != null)
+            .Should()
+            .BeTrue();
+    }
 
-        [Theory]
-        [AutoData]
-        public async Task ResponseWriter_HideExceptions_SetToTrue_ShouldHideExceptionsProperty(
-            HealthReport healthReport)
-        {
-            // Arrange
-            var httpContext = CreateHttpContext();
-            RockLibHealthChecks.HideExceptions = true;
+    [Theory]
+    [AutoData]
+    public static async Task ResponseWriterHideExceptionsSetToTrueShouldHideExceptionsProperty(
+        HealthReport healthReport)
+    {
+        // Arrange
+        var httpContext = CreateHttpContext();
+        RockLibHealthChecks.HideExceptions = true;
 
-            // Act
-            await RockLibHealthChecks.ResponseWriter(httpContext, healthReport);
+        // Act
+        await RockLibHealthChecks.ResponseWriter(httpContext, healthReport).ConfigureAwait(false);
 
-            // Assert
-            var jsonBody = await GetJsonBody(httpContext);
-            var healthCheckEntries = GetHealthCheckEntries(jsonBody);
+        // Assert
+        var jsonBody = await GetJsonBody(httpContext).ConfigureAwait(false);
+        var healthCheckEntries = GetHealthCheckEntries(jsonBody);
 
-            healthCheckEntries
-                .Any(x => x["exception"] != null)
-                .Should()
-                .BeFalse();
-        }
+        healthCheckEntries
+            .Any(x => x["exception"] != null)
+            .Should()
+            .BeFalse();
+    }
 
-        [Theory]
-        [AutoData]
-        public async Task ResponseWriter_HideOutputs_Default_ShouldShowOutputProperty(
-            HealthReport healthReport)
-        {
-            // Arrange
-            var httpContext = CreateHttpContext();
+    [Theory]
+    [AutoData]
+    public static async Task ResponseWriterHideOutputsDefaultShouldShowOutputProperty(
+        HealthReport healthReport)
+    {
+        // Arrange
+        var httpContext = CreateHttpContext();
 
-            // Act
-            await RockLibHealthChecks.ResponseWriter(httpContext, healthReport);
+        // Act
+        await RockLibHealthChecks.ResponseWriter(httpContext, healthReport).ConfigureAwait(false);
 
-            // Assert
-            var jsonBody = await GetJsonBody(httpContext);
-            var healthCheckEntries = GetHealthCheckEntries(jsonBody);
+        // Assert
+        var jsonBody = await GetJsonBody(httpContext).ConfigureAwait(false);
+        var healthCheckEntries = GetHealthCheckEntries(jsonBody);
 
-            healthCheckEntries
-                .Any(x => x["output"] != null)
-                .Should()
-                .BeTrue();
-        }
+        healthCheckEntries
+            .Any(x => x["output"] != null)
+            .Should()
+            .BeTrue();
+    }
 
-        [Theory]
-        [AutoData]
-        public async Task ResponseWriter_HideOutputs_SetToFalse_ShouldShowOutputProperty(
-            HealthReport healthReport)
-        {
-            // Arrange
-            var httpContext = CreateHttpContext();
-            RockLibHealthChecks.HideOutputs = false;
+    [Theory]
+    [AutoData]
+    public static async Task ResponseWriterHideOutputsSetToFalseShouldShowOutputProperty(
+        HealthReport healthReport)
+    {
+        // Arrange
+        var httpContext = CreateHttpContext();
+        RockLibHealthChecks.HideOutputs = false;
 
-            // Act
-            await RockLibHealthChecks.ResponseWriter(httpContext, healthReport);
+        // Act
+        await RockLibHealthChecks.ResponseWriter(httpContext, healthReport).ConfigureAwait(false);
 
-            // Assert
-            var jsonBody = await GetJsonBody(httpContext);
-            var healthCheckEntries = GetHealthCheckEntries(jsonBody);
+        // Assert
+        var jsonBody = await GetJsonBody(httpContext).ConfigureAwait(false);
+        var healthCheckEntries = GetHealthCheckEntries(jsonBody);
 
-            healthCheckEntries
-                .Any(x => x["output"] != null)
-                .Should()
-                .BeTrue();
-        }
+        healthCheckEntries
+            .Any(x => x["output"] != null)
+            .Should()
+            .BeTrue();
+    }
 
-        [Theory]
-        [AutoData]
-        public async Task ResponseWriter_HideOutputs_SetToTrue_ShouldHideOutputProperty(
-            HealthReport healthReport)
-        {
-            // Arrange
-            var httpContext = CreateHttpContext();
-            RockLibHealthChecks.HideOutputs = true;
+    [Theory]
+    [AutoData]
+    public static async Task ResponseWriterHideOutputsSetToTrueShouldHideOutputProperty(
+        HealthReport healthReport)
+    {
+        // Arrange
+        var httpContext = CreateHttpContext();
+        RockLibHealthChecks.HideOutputs = true;
 
-            // Act
-            await RockLibHealthChecks.ResponseWriter(httpContext, healthReport);
+        // Act
+        await RockLibHealthChecks.ResponseWriter(httpContext, healthReport).ConfigureAwait(false);
 
-            // Assert
-            var jsonBody = await GetJsonBody(httpContext);
-            var healthCheckEntries = GetHealthCheckEntries(jsonBody);
+        // Assert
+        var jsonBody = await GetJsonBody(httpContext).ConfigureAwait(false);
+        var healthCheckEntries = GetHealthCheckEntries(jsonBody);
 
-            healthCheckEntries
-                .Any(x => x["output"] != null)
-                .Should()
-                .BeFalse();
-        }
+        healthCheckEntries
+            .Any(x => x["output"] != null)
+            .Should()
+            .BeFalse();
+    }
 
-        private static IEnumerable<JToken> GetHealthCheckEntries(string jsonBody) => JObject.Parse(jsonBody)
-            .SelectToken("checks")
+    private static IEnumerable<JToken> GetHealthCheckEntries(string jsonBody) => 
+        JObject.Parse(jsonBody)!
+            .SelectToken("checks")!
             .Children()
-            .SelectMany(x => x.First);
+            .SelectMany(x => x.First!);
 
-        private static async Task<string> GetJsonBody(DefaultHttpContext httpContext)
+    private static async Task<string> GetJsonBody(DefaultHttpContext httpContext)
+    {
+        httpContext.Response.Body.Seek(0, SeekOrigin.Begin);
+
+        string bodyStr;
+        using (var reader = new StreamReader(httpContext.Response.Body))
         {
-            httpContext.Response.Body.Seek(0, SeekOrigin.Begin);
-
-            string bodyStr;
-            using (var reader = new StreamReader(httpContext.Response.Body))
-            {
-                bodyStr = await reader.ReadToEndAsync();
-            }
-
-            return bodyStr;
+            bodyStr = await reader.ReadToEndAsync().ConfigureAwait(false);
         }
 
-        private static DefaultHttpContext CreateHttpContext()
-        {
-            var httpContext = new DefaultHttpContext();
-            httpContext.Response.Body = new MemoryStream();
-            return httpContext;
-        }
+        return bodyStr;
+    }
+
+    private static DefaultHttpContext CreateHttpContext()
+    {
+        var httpContext = new DefaultHttpContext();
+        httpContext.Response.Body = new MemoryStream();
+        return httpContext;
     }
 }
